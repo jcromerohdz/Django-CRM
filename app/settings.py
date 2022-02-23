dd10af0b80c95abce80f572005063f37d4a80f97
jcromerohdz <jcromerohdz@gmail.com>
2022-02-23 13:07:25 -0800

    Configuration of templates and static folder also add a template adminLTE3 to the app

--- app/settings.py
+++ app/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
--- erp/views.py
+++ erp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def myfirstview(request):
+  data = {
+    "name": 'Christian'
+  }
+  return render(request, 'home.html',  data)
